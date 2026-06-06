465227941c98c1302269e274c234376279c70c80
Developer <dev@example.com>
2026-06-06 21:21:10 +0700

    Fix ALLOWED_HOSTS for Render deployment

--- e_mobix/settings.py
+++ e_mobix/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '').lower() != 'false'
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
